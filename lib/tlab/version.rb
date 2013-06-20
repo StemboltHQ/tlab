8e19c5537681e3b3794af95c37cdc904ecaec673
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2013-06-20 11:09:41 -0700

    Structured data and a bit more markup
    Change-Id: I783a5f0ca5035ff1f8be827b34318218d0ab1cca

--- lib/tlab/version.rb
+++ lib/tlab/version.rb
@@ -1,3 +1,3 @@
 module Tlab
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
--- spec/views/tlab/posts/show.html.erb_spec.rb
+++ spec/views/tlab/posts/show.html.erb_spec.rb
@@ -5,15 +5,17 @@ describe "tlab/posts/show" do
     @post = assign(:post, stub_model(Tlab::Post,
       :title => "Title",
       :preview => "Preview",
+      :body => 'Body',
       :author => nil
     ))
+    view.stub(:can?).and_return true
   end
 
   it "renders attributes in <p>" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/Title/)
-    rendered.should match(/Preview/)
+    rendered.should match(/Body/)
     rendered.should match(//)
   end
 end
